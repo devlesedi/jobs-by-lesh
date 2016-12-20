b561f8e23609ea9092ea6990c5552b70ed31228d
Lesedi Ramahobo <LESH_ent@Lesedis-MBP.lan>
2016-12-20 23:02:20 +0200

    Set title max length to 120

--- src/components/SurveyForm/surveyValidation.js
+++ src/components/SurveyForm/surveyValidation.js
@@ -2,8 +2,8 @@ import memoize from 'lru-memoize';
 import {createValidator, required, maxLength, email} from 'utils/validation';
 
 const surveyValidation = createValidator({
-  title: [required, maxLength(20)],
-  city: [required, maxLength(20)],
+  title: [required, maxLength(120)],
+  city: [required, maxLength(40)],
   companyName: required,
   companyEmail: [required, email]
   // occupation: maxLength(20) // single rules don't have to be in an array
